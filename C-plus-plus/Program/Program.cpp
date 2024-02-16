f0fa0565c7f1f68a26ada4f2f145a583d09f8cba
jonggeolk <jonggeolk2@naver.com>
2024-02-16 22:57:32 +0900

    (2024.01.16)상속

--- C-plus-plus/Program/Program.cpp
+++ C-plus-plus/Program/Program.cpp
@@ -18,13 +18,13 @@ int main()
 	 //상위 클래스의 속성을 하위 클래스가 사용할 수 있도록
 	 //설정해주는 기능입니다.
 
-     Unit unit;
-	 
-	 cout << "unit 오브젝트의 크기 : " << sizeof(unit) << endl;
-	 
-	 Marine marine;
-	 
-	 cout << "marine 오브젝트의 크기 : " << sizeof(marine) << endl;
+  //   Unit unit;
+	 //
+	 //cout << "unit 오브젝트의 크기 : " << sizeof(unit) << endl;
+	 //
+	 //Marine marine;
+	 //
+	 //cout << "marine 오브젝트의 크기 : " << sizeof(marine) << endl;
 
 	 //클래스의 상속 관계에서 상위 클래스는 하위 클래스의 속성을 사용할 
 	 //수 없으며, 하위 클래스는 상위 클래스의 메모리를 포함한 상태로
