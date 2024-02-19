b47045fc7b38a647e2603466ee7e9b901ca6f3f9
jonggeolk <jonggeolk2@naver.com>
2024-02-19 21:45:03 +0900

    (2024.02.19) 다중상속

---
+++ C-plus-plus/Class6th (Encapsuleltion)/Source.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "../Program/Phone.h"
+
+
+using namespace std;
+
+int main()
+{
+#pragma region 캡슐화
+	// Phone phone1;
+	// 
+	// phone1.SetVolume(10);
+	// 
+	// cout << "phone1의 volume 값은 : " << phone1.GetVolume() << endl;
+#pragma endregion
+
+	return 0;
+}
--- C-plus-plus/Program/Program.cpp
+++ C-plus-plus/Program/Program.cpp
@@ -1,18 +1,97 @@
 ﻿#include <iostream>
-#include "Phone.h"
+#include <stdarg.h>
 #include "Marine.h"
+#include "Unit.h"
 
 using namespace std;
- 
+
+class Mouse
+{
+public:
+	Mouse()
+	{
+		cout << "Create Mouse" << endl;
+			
+	}
+	~Mouse()
+	{
+		cout << "Realase Mouse" << endl;
+	}
+	void OnDrag()
+	{
+		cout << "Drag" << endl;
+	}
+	void Connect()
+	{
+		cout << "Connect Mouse" << endl;
+	}
+};
+
+class Keybord
+{
+public:
+	Keybord()
+	{
+		cout << "Create Keybord" << endl;
+	}
+	~Keybord()
+	{
+		cout << "Realase Keybord" << endl;
+	}
+	void Typing()
+	{
+		cout << "Typing" << endl;
+	}
+	void Connect()
+	{
+		cout << "Keybord Connect" << endl;
+	}
+};
+
+class Computer : public Mouse, public Keybord
+{
+public:
+
+	Computer()
+	{
+		cout << "Create Computer" << endl;
+	}
+	~Computer()
+	{
+		cout << "Release Conputer" << endl;
+	}
+	void Use()
+	{
+		Typing();
+		OnDrag();
+
+		Mouse::Connect();
+		Keybord::Connect();
+	}
+};
+
+// Debug(2,10,20)
+// Debuf(3,10,20,30)
+
+void Debug(int count,...) // ... <- 가변길이 매개변수 문법
+{
+	// va_list  : 가변 인자 목록으로 가변 인자의
+	//			  메모리 주소를 저장하는 포인터입니다.
+
+	// va_start : 가변 인자의 시작 주소를 가져오는 변수입니다.
+
+	// va_arg   : 가변 인자 포인터에서 특정 자료형의 크기만큼 
+	//            이동하는 변수이고, 값을 반환하는 변수입니다.
+
+	// va_end   : 가변 인자 처리가 끝났을 때 포인터를 NULL로
+	//	          초기화 하는 변수입니다.
+
+
+
+}
+
 int main()
 {
-#pragma region 캡슐화
-	// Phone phone1;
-	// 
-	// phone1.SetVolume(10);
-	// 
-	// cout << "phone1의 volume 값은 : " << phone1.GetVolume() << endl;
-#pragma endregion
 
 #pragma region 상속
 	 //상위 클래스의 속성을 하위 클래스가 사용할 수 있도록
@@ -31,6 +110,26 @@ int main()
 	 //메모리의 크기가 결정됩니다.
 
 #pragma endregion
+
+#pragma region 다중 상속
+	// 하나의 하위 클래스가 여러 개의 상위 클래스를
+	// 상속 받는 것입니다.
+
+	//Computer computer;
+	//
+	//computer.Use();
+
+	// 다중 상속은 여러 개의 상위 클래스에 중복되는 속성이
+	// 존재할 수 있기 때문에 범위 지정 연산자를 통해 상위 
+	// 클래스의 이름을 성너하고 속성을 사용해야 합니다.
+#pragma endregion
+
+#pragma region 가변길이 매개변수
+
+
+
+#pragma endregion
+
 
 
 
